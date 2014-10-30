db9693a647b4203222734f651febc37fbe05d68a
Danut Chereches <load.runner@weednet.ro>
2014-10-30 21:08:44 +0200

    added while bg on loading scene

--- Classes/LoadingScene.cpp
+++ Classes/LoadingScene.cpp
@@ -9,15 +9,9 @@ void LoadingScene::onEnter()
 	cocos2d::Scene::onEnter();
 	
 	// add background to current scene
-//	Sprite *background = Sprite::create("splash.png");
-//	Size visibleSize = Director::getInstance()->getVisibleSize();
-//	Point origin = Director::getInstance()->getVisibleOrigin();
-//	background->setPosition(origin.x + visibleSize.width/2, origin.y + visibleSize.height/2);
-//	this->addChild(background);
+	cocos2d::LayerColor* bg = cocos2d::LayerColor::create(cocos2d::Color4B::WHITE);
+	this->addChild(bg);
 	
-//	cocos2d::LayerColor* bg = cocos2d::LayerColor::create(cocos2d::Color4B::WHITE);
-//	this->addChild(bg);
-//	
 //	auto logo = cocos2d::Sprite::create("logo.png");
 //	cocos2d::Size visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
 //	cocos2d::Point origin = cocos2d::Director::getInstance()->getVisibleOrigin();
